87c5932e686231cdcfefebbea52349292ae7794f
Obispodino <63376388+Obispodino@users.noreply.github.com>
2022-04-23 16:11:13 +0100

    Add the files for R

---
+++ Lab1.R
@@ -0,0 +1,11 @@
+library (datasets)
+data(iris)
+View(iris)
+
+unique(iris$Species)
+
+install.packages(c("GGally","ggplot2"))
+
+library(GGally) 
+ggpairs(iris, mapping=ggplot2::aes(colour = Species))
+
